bdc0df2b84a0a844009d94ec0451ac9d0ea3f278
kathcherish <kathleen.cherish11@gmail.com>
2025-01-11 09:47:49 +0700

    Add files via upload

---
+++ hello_website.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.title("Welcome to This Page! *octo[new]ber yey*")
+button = st.button("Look at this new feature")
+button2 = st.button("Another feature!")
+button3 = st.button("CURIOUS ABOUT ANOTHER SECRET?")
+
+if button:
+    st.balloons()
+
+if button2:
+    st.snow()
+    
+if button3:
+    st.write("hmmm, you can't know this! see ya on novem[new]ber!")
+    st.balloons()
+    st.snow()
+    
+
+
+
+
