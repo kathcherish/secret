dcf9d5e998d1f796903c94f3d1a0767ec9163314
kathcherish <kathleen.cherish11@gmail.com>
2025-01-25 09:48:29 +0700

    Update about_dataframe.py

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -5,10 +5,11 @@ data = pd.read_excel('./pages/source.xlsx')
 unique_category = data['Category'].unique()
 
 selected_category = st.multiselect("Select Category",options = unique_category)
-
 criteria1 = data['Category'].isin(selected_category)
 
-criteria2 = data['Store'] == 'Indomaret'
+selected_store = st.multiselect("Select Store",options = unique_store)
+criteria2 = data['Store'].isin(selected_store)
+
 criteria3 = data['Price'] > 25000
 criteria4 = (data['Price'] > 15000) & (data['Price'] < 50000)
 criteria5 = (criteria1) & (criteria2) & (criteria4)
