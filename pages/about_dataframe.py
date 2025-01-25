c3edcd7a8968c9211b4074273f6bf7677dbc4b6e
kathcherish <kathleen.cherish11@gmail.com>
2025-01-25 09:57:06 +0700

    Update about_dataframe.py

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -6,17 +6,17 @@ unique_category = data['Category'].unique()
 unique_store = data['Store'].unique()
 
 selected_category = st.multiselect("Select Category",options = unique_category)
-criteria1 = data['Category'].isin(selected_category)
-
 selected_store = st.multiselect("Select Store",options = unique_store)
+
+criteria1 = data['Category'].isin(selected_category)
 criteria2 = data['Store'].isin(selected_store)
+join_criteria = (criteria1) & (criteria2)
 
 criteria3 = data['Price'] > 25000
 criteria4 = (data['Price'] > 15000) & (data['Price'] < 50000)
 criteria5 = (criteria1) & (criteria2) & (criteria4)
 
-data = data[criteria1]
-data = data[criteria2]
+data = data[join_criteria]
 
 #print(data[criteria5])
 print(data[criteria5].sort_values('Price',ascending=True))
