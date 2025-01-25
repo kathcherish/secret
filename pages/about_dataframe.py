8682df65ed9559e9391c048d4339b6d6ccda0d93
kathcherish <kathleen.cherish11@gmail.com>
2025-01-25 09:45:24 +0700

    Update about_dataframe.py

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -2,15 +2,18 @@ import pandas as pd
 import streamlit as st
 
 data = pd.read_excel('./pages/source.xlsx')
-unique_category = data['category'].unique()
+unique_category = data['Category'].unique()
 
 selected_category = st.multiselect("Select Category",options = unique_category)
 
-criteria1 = data ['Category'] == 'Food'
+criteria1 = data ['Category'].isin(selected category)
+
 criteria2 = data['Store'] == 'Indomaret'
 criteria3 = data['Price'] > 25000
 criteria4 = (data['Price'] > 15000) & (data['Price'] < 50000)
 criteria5 = (criteria1) & (criteria2) & (criteria4)
+
+data = data[criteria1]
 
 #print(data[criteria5])
 print(data[criteria5].sort_values('Price',ascending=True))
