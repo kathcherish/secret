0b43d6589c12d528d90525fac92dde75ca8b6e41
kathcherish <kathleen.cherish11@gmail.com>
2025-01-28 20:55:26 +0700

    Update about_dataframe.py

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -4,8 +4,8 @@ import streamlit as st
 data = pd.read_excel('./pages/source.xlsx')
 unique_category = data['Category'].unique()
 unique_store = data['Store'].unique()
-minimum_price = data['price'].min()
-maximum_price = data['price'].max()
+minimum_price = data['Price'].min()
+maximum_price = data['Price'].max()
 
 selected_category = st.multiselect("Select Category",options=unique_category,default=unique_category)
 selected_store = st.multiselect("Select Store",options=unique_storename,default=unique_storename)
