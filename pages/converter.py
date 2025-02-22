4df024e6efef909b750045ccff5c6c02d09f750f
kathcherish <kathleen.cherish11@gmail.com>
2025-02-22 09:58:31 +0700

    Update converter.py

--- pages/converter.py
+++ pages/converter.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
 conversion_factors = {
-  'distance': {('mm':1), 
-               ('cm':0,1),
-               ('m':0.01)},
+  'distance': {'mm':1), 
+               'cm':0,1),
+               'm':0.01)},
 
 'weight':None,
 'time':None
 }
 
 #selection category
-category = st.radio("Select category",options=conversion_factor.keys())
+category _list = list(conversion_factor.keys())
+category = st.radio("Select category",options=category_list)
