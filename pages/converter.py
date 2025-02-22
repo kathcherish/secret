661457b5de4a64a28e04d636dc99c707b2c904bc
kathcherish <kathleen.cherish11@gmail.com>
2025-02-22 10:07:45 +0700

    Update converter.py

--- pages/converter.py
+++ pages/converter.py
@@ -11,7 +11,11 @@ conversion_factors = {
 
 #selection category
 category_list = list(conversion_factors.keys())
-
-
 category = st.radio("Select category",options=category_list)
+
 base_unit_list = list(conversion_factors[category].keys())
+base = st.radio("Select Base",options=base_unit_list)
+
+target_unit_list = list(conversion_factors[category].keys())
+target = st.radio("Select Target",options=target_unit_list)
+
