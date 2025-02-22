1587d8f09153b790ecc074f73c70684054875778
kathcherish <kathleen.cherish11@gmail.com>
2025-02-22 10:10:52 +0700

    Update converter.py

--- pages/converter.py
+++ pages/converter.py
@@ -9,13 +9,22 @@ conversion_factors = {
 'time':None
 }
 
+col1,col2,col3,col4,col5 = st.columns(5)
+
 #selection category
-category_list = list(conversion_factors.keys())
-category = st.radio("Select category",options=category_list)
+with col1:
+  category_list = list(conversion_factors.keys())
+  category = st.radio("Select category",options=category_list)
+  st.write(category_list)
 
-base_unit_list = list(conversion_factors[category].keys())
-base = st.radio("Select Base",options=base_unit_list)
+with col2:
+  base_unit_list = list(conversion_factors[category].keys())
+  base_unit = st.radio("Select Base",options=base_unit_list)
+  st.write(base_unit_list)
 
-target_unit_list = list(conversion_factors[category].keys())
-target = st.radio("Select Target",options=target_unit_list)
+with col3:
+  target_unit_list = list(conversion_factors[category].keys())
+  target_unit = st.radio("Select Target",options=target_unit_list)
+  st.write(target_unit_list)
+
 
