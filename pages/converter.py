aeecee782c687662e5b7ef063fc17d6fc8dce8e4
kathcherish <kathleen.cherish11@gmail.com>
2025-02-22 09:54:21 +0700

    Update and rename pages1 to converter.py

---
+++ pages/converter.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+conversion_factors = {
+  'distance': ('mm':1), 
+  'weight':('cm':0,1)
+  'time':('m':0.01)}
+
+'weight':pass,
+'time':pass
+}
+
+#selection category
+category = st.radio("Select category",options=conversion_factor.keys())
