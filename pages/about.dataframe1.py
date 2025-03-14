5cdd9b57c4af5e68575a154a4226b6fdf1211719
kathcherish <kathleen.cherish11@gmail.com>
2025-03-14 18:50:41 +0700

    Update about.dataframe1.py

--- pages/about.dataframe1.py
+++ pages/about.dataframe1.py
@@ -34,6 +34,7 @@ for i in range(data_count):
         product_name = data.iloc[i]['Name']
         st.image(product_picture,width = 250)
         st.write(product_name)
+        st.write(product_price)
 
     #product_name = data.iloc[i]['Name']
     #st.write(product_name)
